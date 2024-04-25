52966d1a6fe9cca9578d05acfd243bd867d530c2
Nghiep <me@nghiep.dev>
2024-04-25 20:22:05 +0700

    Clean repository

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: '#FFD900',
+        secondary: '#4EC5F1',
+      },
+      dropShadow: {
+        normal: '0 4px 4px rgba(0, 0, 0, 0.25)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-multi')],
 };
 export default config;
